de299808fe6008a2c197051da17d9daf9512ed31
Robert O'Callahan <robert@ocallahan.org>
2016-04-25 20:24:11 +1200

    Update

--- all-bench.sh
+++ all-bench.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-bash rr-bench-htmltest.sh >& ~/tmp/output-htmltest
-bash rr-bench-make.sh >& ~/tmp/output-make
-bash rr-bench-cp.sh >& ~/tmp/output-cp
-bash rr-bench-octane.sh >& ~/tmp/output-octane
-bash rr-bench-sambatest.sh >& ~/tmp/output-sambatest
+bash rr-bench-htmltest.sh >& output-htmltest
+bash rr-bench-make.sh >& output-make
+bash rr-bench-cp.sh >& output-cp
+bash rr-bench-octane.sh >& output-octane
+bash rr-bench-sambatest.sh >& output-sambatest
 
